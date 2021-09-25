a29295330fce8f24ebb37213c2f349d3be8de249
Phan Minh Tài <taipm.vn@gmail.com>
2021-09-26 00:29:29 +0700

    Add schedule

--- questions/clock.py
+++ questions/clock.py
@@ -8,17 +8,5 @@ print('Schedule')
 def timed_job():
     print('Schedule')
     Bot.send_message('Trong giờ')
-    # if(IsRunTime()):
-    #     Bot.send_message('Trong giờ')
-    # else:
-    #     Bot.send_message('Ngoài giờ')
 
-
-#job = scheduler.add_job(myfunc, 'interval', minutes=2)
-
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
-
-
-sched.start()
+#sched.start()

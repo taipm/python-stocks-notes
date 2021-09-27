2e2c50d72d96f99e542f44717f25646cf2a1f23f
Phan Minh Tài <taipm.vn@gmail.com>
2021-09-27 22:08:12 +0700

    Adjust home view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from main.models import Question
+from main.models import Comment, Question
 from users.models import User
 from django.core.paginator import Paginator
 
@@ -7,6 +7,7 @@ def homeFeedView(request):
     current_user = request.user
     
     questions = Question.objects.filter(points__gt=-2, hidden=False).order_by('-created')
+    comments = Comment.objects.all().order_by('-created')[0:1]
     paginator = Paginator(questions, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -15,7 +16,8 @@ def homeFeedView(request):
     context = {
         'current_user': current_user,
         'page_obj': page_obj,
-        'questions_exist': questions_exist
+        'questions_exist': questions_exist,
+        'comments':comments
     }
     return render(request, 'home.html', context)
 
